03e334a3e5d5e46e74ce175b60068de8f1cbf9fa
Trevor Guleserian <trevorguleserian@gmail.com>
2020-05-09 16:51:14 -0500

    Added Comments

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,41 +1,50 @@
+# Importing modules
 import os
 import csv
 
 
-
+# Setting path for PyBank.csv
 csvpath = os.path.join("Resources", "PyBank.csv")
 
+# Establishing counter for total monthcount and Profit_Losses
 Monthcount = 0
 Profit_Losses = 0.0
+
+# Setting empty array values to add into later
 date = []
 profit = []
 profit_month = []
 
 
 
-
+# Opening path
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-
+    # Stating header of data
     Header = next(csvreader)
 
+    # Initiating iteration to add values to empty arrays listed above
     for row in csvreader:
 
         profit.append(int(row[1]))
         date.append(row[0])
 
+        # Month counter mechanism
         Monthcount = Monthcount + 1  
 
         
-
+    # iterating through profit to find total profit
     for x in profit:
         Profit_Losses = sum(profit)
 
+    # Calculating average change
     Average_Change = round(Profit_Losses/Monthcount,2) 
 
+    # Iterating through profit to find the length of the profit as a range in order to find biggest change and loss 
     for i in range(len(profit)-1):
         profit_month.append(profit[i-1] - profit[i])
 
+    # Using the index of the greatest increase and decrease to find the date accociated with the change
     Max = profit_month.index(max(profit_month)) + 1
     Min = profit_month.index(min(profit_month)) + 1 
 
@@ -43,7 +52,7 @@ with open(csvpath) as csvfile:
 
     
 
-    
+    # Printing findings
     print("Financial Analysis")
     print("------------------------")
     print(f"Total Months: {Monthcount}")
@@ -52,6 +61,7 @@ with open(csvpath) as csvfile:
     print(f"Greatest increase in profits: {date[Max]} ({max(profit_month)})")
     print(f"Greatest increase in profits: {date[Min]} ({min(profit_month)})")
 
+    # Output path written as txt file
     output_path = os.path.join("Analysis", "PyBank_Analysis")
 with open(output_path, "w") as txtfile:
     
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,29 +1,34 @@
+# Importing modules
 import os
 import csv
 
-
+# Setting path for PyPoll.csv
 csvpath = os.path.join("Resources", "PyPoll.csv")
 
+# Setting variable for counter in iteration below
 Vote_tally = 0
-Canidate = []
+
+# Establising lists to hold each vote for Canidate
 Khan = []
 Correy = []
 Li = []
 OTooley = []
 
-
+# Opening csv path in order to work within its perameters
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     
-    
+    # Establishing the header of the csvfile
     Header = next(csvreader)
 
     
-
+    # Starting an iteration through our csv data for each row
     for row in csvreader:
-        
+
+        # Mechanism to count total votes
         Vote_tally = Vote_tally + 1
 
+        # Mechanism to add votes each canidate received through an conditional satement
         if row[2] == "Khan":
             Khan.append(row[2])                       
         elif row[2] == "Correy":
@@ -32,20 +37,24 @@ with open(csvpath) as csvfile:
             Li.append(row[2])
         elif row[2] == "O'Tooley":
             OTooley.append(row[2])
-        
+
+    # Evaluating the ammount of votes each canidate received   
     KhanTotal = len(Khan)
     CorreyTotal = len(Correy)
     LiTotal = len(Li)       
     OTooleyTotal = len(OTooley)
 
+    # Calculating percentage of the total vote each canidate received
     KhanPercentage = (KhanTotal/Vote_tally) * 100
     CorreyPercentage = (CorreyTotal/Vote_tally) * 100
     LiPercentage = (LiTotal/Vote_tally) * 100
     OTooleyPercentage = (OTooleyTotal/Vote_tally) * 100
 
+    # Storing the total vote for each canidate in an array in order to find popular vote   
     PopularVoteIndex = [KhanTotal,CorreyTotal,LiTotal,OTooleyTotal]
     PopularVoteCount = max(PopularVoteIndex)
 
+    # Iterating through the popular vote index to find who had the most votes
     for VoteCount in PopularVoteIndex:
         if PopularVoteCount == KhanTotal:
             Winner = "Khan" 
@@ -56,7 +65,7 @@ with open(csvpath) as csvfile:
         elif PopularVoteCount == OTooleyTotal:
             Winner = "O'Tooley"
 
-
+    #Printing out each value that we calculated
     print("Election Results")
     print("------------------")
     print(f"Total Votes: {Vote_tally}")
@@ -67,7 +76,7 @@ with open(csvpath) as csvfile:
     print(f"O'Tooley: {OTooleyPercentage} ({OTooleyTotal})")
     print("------------------")
     print(f"Winner: {Winner}")
-
+# Establishing path for file to be written out
 output_path = os.path.join("Analysis", "PyPoll_Analysis")
 with open(output_path, "w") as txtfile:
     
@@ -81,6 +90,7 @@ with open(output_path, "w") as txtfile:
     txtfile.write(f"O'Tooley: {OTooleyPercentage} ({OTooleyTotal})\n")
     txtfile.write("------------------\n")
     txtfile.write(f"Winner: {Winner}\n")
+
 
 
     
